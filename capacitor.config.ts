523802c63ec6fefb1469f38328cd61f2ca4020e8
spider-calvin <calvindas7@gmail.com>
2025-08-21 00:37:25 +0530

    capacitor and the supabase setup done

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chitragupth.app',
+  appName: 'ChitraGupta',
+  webDir: 'out'
+};
+
+export default config;
